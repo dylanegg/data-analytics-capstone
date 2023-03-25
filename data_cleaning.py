240fd7fbdeb2ad65013b8984a14e9dbd883075be
Dylan Eggemeyer <dseggemeyer@gmail.com>
2023-03-25 12:16:17 -0500

    Original Commit: Adding data sources

---
+++ data_cleaning.py
@@ -0,0 +1,12 @@
+"""
+data_cleaning.py
+Created on 3/25/2023
+Author: Dylan Eggemeyer
+"""
+
+import pandas as pd
+
+data = pd.read_csv('infant_mortality_combined_dataset.csv')
+
+data.colummns
+data.describe()
